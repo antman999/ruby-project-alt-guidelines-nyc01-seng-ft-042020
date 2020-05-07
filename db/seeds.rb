495d67d8f662a457ad6aa7b5f03ce0ce58698e7c
github username <github email address>
2020-05-07 16:07:39 -0400

    Final

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,20 @@
+# #only data needed is a seed file 
+
+Game.create(title:"Grand theft auto 5", platform:"Multi -platform", developer:"rockstar games", genre:"Sandbox", esrb_rating:"M")
+Game.create(title:"Minecraft", platform:"Multi -platform", developer:"Mojang", genre:"Family", esrb_rating:"E")
+Game.create(title:"PlayerUnknown's Battlegrounds", platform:"Multi -platform", developer:"PUBG Corporation", genre:"Shooter", esrb_rating:"M")
+Game.create(title:"Super Mario Bros", platform:"Multi -platform", developer:"Nintendo", genre:"family", esrb_rating:"E")
+Game.create(title:"Red Dead Redemption 2", platform:"Multi -platform", developer:"Rockstar games", genre:"sandbox", esrb_rating:"M")
+Game.create(title:"Grand Theft Auto San Andreas", platform:"Multi -platform", developer:"Rockstar games", genre:"sandbox", esrb_rating:"M")
+Game.create(title:"Call of Duty Modern Warfare 3", platform:"Multi -platform", developer:"Activision", genre:"shooter", esrb_rating:"M")
+Game.create(title:"Call of Duty Black Ops", platform:"Multi -platform", developer:"Activision", genre:"shooter", esrb_rating:"M")
+Game.create(title:"Call of Duty Black Ops 2", platform:"Multi -platform", developer:"Activision", genre:"shooter", esrb_rating:"M")
+Game.create(title:"Grand Theft Auto 4", platform:"Multi -platform", developer:"Rockstar Games", genre:"sandbox", esrb_rating:"M")
+Game.create(title:"Call of Duty Modern Warfare 2", platform:"Multi -platform", developer:"Activision", genre:"shooter", esrb_rating:"M")
+Game.create(title:"FIFA 18", platform:"Multi -platform", developer:"Ea sports", genre:"Sports", esrb_rating:"E")
+Game.create(title:"2K20", platform:"Multi -platform", developer:"2k Games", genre:"Sports", esrb_rating:"E")
+Game.create(title:"2K19", platform:"Multi -platform", developer:"2k Games", genre:"sports", esrb_rating:"E")
+Game.create(title:"FIFA 20", platform:"Multi -platform", developer:"EA sports", genre:"sports", esrb_rating:"E")
+Game.create(title:"Fortnite", platform:"Multi -platform", developer:"Epic Games", genre:"shooter", esrb_rating:"T")
 
 
